2f18a8294404cd33bcd46b53f10006c5fa692f1c
航飞 张 <3225879907@qq.com>
2023-05-09 01:54:32 +0800

    添加type字段

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -7,53 +7,65 @@ import {Entity, model, property} from '@loopback/repository';
 
 @model()
 export class Todo extends Entity {
-  @property({
-    type: 'number',
-    id: true,
-    generated: false,
-  })
-  id?: number;
+    @property({
+        type: 'number',
+        id: true,
+        generated: false,
+    })
+    id?: number;
 
-  @property({
-    type: 'string',
-    required: true,
-  })
-  title: string;
+    @property({
+        type: 'string',
+        required: true,
+    })
+    title: string;
 
-  @property({
-    type: 'string',
-  })
-  desc?: string;
+    @property({
+        type: 'string',
+    })
+    desc?: string;
 
-  @property({
-    type: 'boolean',
-  })
-  isComplete?: boolean;
+    @property({
+        type: 'boolean',
+    })
+    isComplete?: boolean;
 
-  @property({
-    type: 'string',
-  })
-  remindAtAddress?: string; // address,city,zipcode
+    @property({
+        type: 'string',
+    })
+    remindAtAddress?: string; // address,city,zipcode
 
-  // TODO(bajtos) Use LoopBack's GeoPoint type here
-  @property({
-    type: 'string',
-  })
-  remindAtGeo?: string; // latitude,longitude
+    // TODO(bajtos) Use LoopBack's GeoPoint type here
+    @property({
+        type: 'string',
+    })
+    remindAtGeo?: string; // latitude,longitude
 
-  @property({
-    type: 'any',
-  })
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  tag?: any;
+    @property({
+        type: 'any',
+    })
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    tag?: any;
 
-  constructor(data?: Partial<Todo>) {
-    super(data);
-  }
+
+    @property({
+        type: 'string',
+    })
+    type?: string;
+
+    constructor(data?: Partial<Todo>) {
+        super(data);
+    }
 }
 
 export interface TodoRelations {
-  // describe navigational properties here
+    // describe navigational properties here
 }
+
+export type TypeCountInfo = {
+    type: string,
+    count: number
+}
+
 
 export type TodoWithRelations = Todo & TodoRelations;
